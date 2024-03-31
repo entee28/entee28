186dadcb4d3518844db49f706d92cc69349947de
Nhật Thành <85399198+entee28@users.noreply.github.com>
2024-03-31 22:21:46 +0700

    Update ubuntu-sonarqube-init.sh

--- ubuntu-sonarqube-init.sh
+++ ubuntu-sonarqube-init.sh
@@ -22,7 +22,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Running Sonarqube
 docker volume create --name sonarqube_data
